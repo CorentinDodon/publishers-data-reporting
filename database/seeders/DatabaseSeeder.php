a9165d80962d28b1078fe1b40cb329f2005d8f27
Corentin Dodon <corentin.dodon@gmail.com>
2023-03-18 22:20:07 +0100

    update DatabaseSeeder to init data of new tables

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\CurrencyRate;
+use App\Models\Publisher;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        Publisher::factory(10)->hasReportingData(100)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        CurrencyRate::factory()->count(155)->create();
+        // update base currency USD rate to 1
+        DB::table('currency_rate')->where('code', 'USD')->update(['rate' => 1]);
     }
 }
